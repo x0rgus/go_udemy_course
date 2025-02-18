9e06e9ce398a19caa0c7d4fbcceb9ee52fd98be3
x0rgus <lucas.pr1@hotmail.com>
2025-02-18 11:59:21 -0300

    :bookmark: aula 10 finalizada

--- Structs/structs.go
+++ Structs/structs.go
@@ -9,4 +9,16 @@ type usuario struct {
 
 func main() {
 	fmt.Println("Arquivo structs")
+
+	var u usuario
+	u.nome = "Davi"
+	u.idade = 21
+	fmt.Println(u)
+
+	usuario2 := usuario{"João", 25}
+	fmt.Println(usuario2)
+
+	// para declarar um struct sem passar todos os valores é necessario usar argumentos nomeados
+	usuario3 := usuario{nome: "Maria"}
+	fmt.Println(usuario3)
 }
