29b7d5f4b2a13f49c972a39c72bd02b77813cdf8
x0rgus <lucas.pr1@hotmail.com>
2025-02-18 12:00:03 -0300

    :bookmark: aula 10 finalizada

--- Structs/structs.go
+++ Structs/structs.go
@@ -2,23 +2,32 @@ package main
 
 import "fmt"
 
+// Defining the 'usuario' struct with fields 'nome', 'idade', and 'endereco'
 type usuario struct {
-	nome  string
-	idade uint8
+	nome     string
+	idade    uint8
+	endereco endereco // It is possible to create a struct inside another struct
+}
+
+// Defining the 'endereco' struct with fields 'logradouro' and 'numero'
+type endereco struct {
+	logradouro string
+	numero     uint8
 }
 
 func main() {
-	fmt.Println("Arquivo structs")
+	fmt.Println("Arquivo structs") // Printing a message to indicate the file
 
 	var u usuario
-	u.nome = "Davi"
+	u.nome = "Davi" // Assigning values to the 'usuario' struct fields
 	u.idade = 21
-	fmt.Println(u)
+	fmt.Println(u) // Printing the 'usuario' struct
 
-	usuario2 := usuario{"João", 25}
-	fmt.Println(usuario2)
+	enderecoExemplo := endereco{"Rua dos Bobos", 0}  // Creating an 'endereco' struct instance
+	usuario2 := usuario{"João", 25, enderecoExemplo} // Creating a 'usuario' struct instance with an 'endereco' instance
+	fmt.Println(usuario2)                            // Printing the 'usuario' struct
 
-	// para declarar um struct sem passar todos os valores é necessario usar argumentos nomeados
+	// To declare a struct without passing all values, it is necessary to use named arguments
 	usuario3 := usuario{nome: "Maria"}
-	fmt.Println(usuario3)
+	fmt.Println(usuario3) // Printing the 'usuario' struct
 }
