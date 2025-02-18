8b18eb407551af02e71bda78bf256dac89c17ab1
x0rgus <lucas.pr1@hotmail.com>
2025-02-18 14:23:53 -0300

    :bookmark: aula 21 finalizada

--- 14 - Funçoes avançadas/14.3 - Funçoes anonimas/anonimas.go
+++ 14 - Funçoes avançadas/14.3 - Funçoes anonimas/anonimas.go
@@ -6,4 +6,13 @@ func main() {
 	func() { // anonymous function without parameters and return
 		fmt.Println("Olá, mundo!")
 	}() //"()" calls the function immediately
+
+	func(texto string) { // anonymous function with parameter
+		fmt.Println(texto)
+	}("Olá, mundo! isso é um parametro!")
+
+	retorno := func(texto string) string { // anonymous function with parameter and return
+		return fmt.Sprintf("Recebido -> %s", texto) // formats the string and returns it
+	}("Olá, mundo! isso é um parametro retornado!")
+	fmt.Println(retorno)
 }
