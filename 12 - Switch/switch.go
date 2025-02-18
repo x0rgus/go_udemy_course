3ee3be55b9128c5514e8f3be96f2cefaddabfc40
x0rgus <lucas.pr1@hotmail.com>
2025-02-18 13:30:16 -0300

    :bookmark: aula 17 finalizada

--- 12 - Switch/switch.go
+++ 12 - Switch/switch.go
@@ -3,26 +3,34 @@ package main
 import "fmt"
 
 func diaDaSemana(numero int) string { // Function that returns the day of the week according to the number passed as a parameter
+	var diaDaSemana string
 	switch numero {
 	case 1:
-		return "Domingo"
+		diaDaSemana = "Domingo"
+		fallthrough // The 'fallthrough' keyword is used to execute the next case
 	case 2:
-		return "Segunda-feira"
+		diaDaSemana = "Segunda-feira"
 	case 3:
-		return "Terça-feira"
+		diaDaSemana = "Terça-feira"
 	case 4:
-		return "Quarta-feira"
+		diaDaSemana = "Quarta-feira"
 	case 5:
-		return "Quinta-feira"
+		diaDaSemana = "Quinta-feira"
 	case 6:
-		return "Sexta-feira"
+		diaDaSemana = "Sexta-feira"
 	case 7:
-		return "Sábado"
+		diaDaSemana = "Sábado"
 	default:
-		return "Número inválido"
+		diaDaSemana = "Número inválido"
 	}
+	return diaDaSemana
 }
 
 func main() {
 	fmt.Println("Switch")
+	dia := diaDaSemana(3)
+	fmt.Println(dia)
+
+	dia2 := diaDaSemana(1)
+	fmt.Println(dia2) // The 'fallthrough' keyword makes the 'diaDaSemana' variable receive the value of the next case
 }
