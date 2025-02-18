ec23ee722d39205423aaf102f5bc4c50d158d161
x0rgus <lucas.pr1@hotmail.com>
2025-02-18 12:12:14 -0300

    :bookmark: aula 11 finalizada

--- 7 - Herança/heranca.go
+++ 7 - Herança/heranca.go
@@ -4,9 +4,9 @@ import "fmt"
 
 type pessoa struct {
 	nome      string
+	sobrenome string
 	idade     uint8
 	altura    uint8
-	sobrenome string
 }
 type estudante struct {
 	pessoa    // passes all fields from 'pessoa' struct to 'estudante' struct (composition)
@@ -16,4 +16,10 @@ type estudante struct {
 
 func main() {
 	fmt.Println("Arquivo heranca") // Printing a message to indicate the file
+	p1 := pessoa{"joão", "pedro", 20, 178}
+	fmt.Println(p1)
+
+	e1 := estudante{p1, "engenharia", "USP"} // Creating a 'estudante' struct instance with a 'pessoa' instance
+	fmt.Println(e1)
+	fmt.Println(e1.nome) // Accessing the 'nome' field from the 'pessoa' struct
 }
