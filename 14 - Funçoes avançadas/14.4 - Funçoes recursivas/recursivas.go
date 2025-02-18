24be1d54fc530d7040e196a3607f120350b058fa
x0rgus <lucas.pr1@hotmail.com>
2025-02-18 15:12:12 -0300

    :sparkles: aula 22 finalizada

---
+++ 14 - Funçoes avançadas/14.4 - Funçoes recursivas/recursivas.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func fibonacci(posicao uint) uint {
+	if posicao <= 1 {
+		return posicao
+	}
+	return fibonacci(posicao-2) + fibonacci(posicao-1)
+}
+func main() {
+	// Recursive function
+	posicao := uint(12)
+
+	for i := uint(1); i <= posicao; i++ {
+		fmt.Println(fibonacci(i))
+	}
+	fmt.Println(fibonacci(posicao))
+}
